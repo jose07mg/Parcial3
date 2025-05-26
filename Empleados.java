f54884348618728afa5d941fa05193d15bd2c940
jose07mg <josemunozgalvez4@gmail.com>
2025-05-26 11:12:56 +0200

    separacion codigo en clase nueva empleados

---
+++ Empleados.java
@@ -0,0 +1,31 @@
+public class Empleados {
+    public Empleado[] lista;
+    private int contador;
+
+    public Empleados(int capacidad) {
+        lista = new Empleado[capacidad];
+        contador = 0;
+    }
+
+    public void darDeAlta(String nombre, String cargo, double salario) {
+        if (contador < lista.length) {
+            lista[contador] = new Empleado(nombre, cargo, salario);
+            contador++;
+        } else {
+            System.out.println("No se pueden agregar más empleados. Capacidad máxima alcanzada.");
+        }
+    }
+
+    public void aumentarSalario(double porcentaje) {
+        for (Empleado empleado : lista) {
+            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
+            empleado.setSalario(nuevoSalario);
+        }
+    }
+
+    public void mostrarEmpleados() {
+        for (Empleado empleado : lista) {
+            System.out.println(empleado);
+        }
+    }
+}
