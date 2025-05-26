329530a98cf866108855115e963c51adf8a21647
jose07mg <josemunozgalvez4@gmail.com>
2025-05-26 10:12:13 +0200

    cambio getters/setters y atributosprivados

--- Empleado.java
+++ Empleado.java
@@ -1,7 +1,7 @@
 class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+    private String nombre;
+    private String cargo;
+    private double salario;
 
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
@@ -16,5 +16,32 @@ class Empleado {
                 ", cargo='" + cargo + '\'' +
                 ", salario=" + salario +
                 '}';
+            }
+
+
+    public String getNombre() {
+        return nombre;
     }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+            
 }
